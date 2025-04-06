1ba9c7eee583aaf238dafa24f9a327f750cddfd9
Siwakorn Mahakan <cardeer.dev@gmail.com>
2025-04-06 21:16:17 +0700

    fix: downloader selected nil

--- utils/downloader.lua
+++ utils/downloader.lua
@@ -1,20 +1,20 @@
 ---@type Downloader.Filtered
 local filter = {
     filter = nil,
-    quality = 'normal',
+    quality = 'normal'
 }
 
 if not storage_downloader then
     storage_downloader = {}
 end
 
-if not storage.downloader_selected then
-    storage.downloader_selected = {}
-end
-
 ---@param id number
 ---@return Downloader.Filtered
 function storage_downloader.get(id)
+    if not storage.downloader_selected then
+        storage.downloader_selected = {}
+    end
+
     if not storage.downloader_selected[id] then
         storage.downloader_selected[id] = flib_table.deep_copy(filter)
     end
