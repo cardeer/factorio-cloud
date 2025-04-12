2475efba7bd81f1340b50e2a48461af3327ad5f1
Siwakorn Mahakan <siwakorn.mah@gmail.com>
2025-04-12 14:42:39 +0700

    refactor: use elseif for checking entity name

--- scripts/events/on_player_mined_entity.lua
+++ scripts/events/on_player_mined_entity.lua
@@ -3,8 +3,7 @@ function events.on_player_mined_entity(event)
     local entity = event.entity
     if entity.name == constants.items.cloud_storage_downloader.name then
         storage_downloader.remove(entity.unit_number)
-    end
-    if entity.name == constants.items.cloud_storage_uploader.name then
+    elseif entity.name == constants.items.cloud_storage_uploader.name then
         storage_uploader.remove(entity.unit_number)
     end
 end
